992130c2ba580d04af8a6651cb2384b6c1a2d8ca
jnpark <jnpark@pentasecurity.com>
2019-04-16 01:57:47 +0900

    [mutation, query] city 정의. (테스트 미실행)

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,35 @@
 import { resolver } from 'graphql-sequelize';
 import { createContext, EXPECTED_OPTIONS_KEY } from 'dataloader-sequelize';
 import models from '../models';
+// const resolvers = {
+//     Query: {
+//         cities:() => resolver(models.city)
+//     }
+// };
 const resolvers = {
-    Query: {
-        name:() => resolver(models.city)
-    }
-};
-resolver.contextToOptions = { [EXPECTED_OPTIONS_KEY]: EXPECTED_OPTIONS_KEY };
+        Query: {
+            cities:() => {
+                return models.city.all();
+            },
+            city:(_, args) => {
+                return models.city.find({
+                    where: args
+                })
+            }
+        },
+        Mutation: {
+            createCity: (_,args) =>
+            {
+                const city = models.city.create({
+                    city_id: args.city_id,
+                    city_name: args.city_name,
+                    country_id: args.country_id
+                });
+                return city;
+            }
+        }
+    };
+
+//resolver.contextToOptions = { [EXPECTED_OPTIONS_KEY]: EXPECTED_OPTIONS_KEY };
 
 export default resolvers;
--- models/city.js
+++ models/city.js
@@ -1,11 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 
 class city extends Model {
-    constructor(tableName)
-    {
-      super(tableName, tableName);
-      this.tableName = 'city';
-    }
 }
 
 const schema = {
@@ -17,7 +12,7 @@ const schema = {
 export default (sequelize) => {
   city.init(schema, {
     sequelize,
-    tableName: city.tableName,
+    tableName: 'city',
   });
 
   return city;
